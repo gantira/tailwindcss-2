3ee3f083c7afb26c4e9bdaa187dfa740b911e938
Angga Gantira <angga.gantira@gmail.com>
2021-01-30 08:26:34 +0700

    4 Ring, Apa Itu ?

---
+++ components/Ring.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Ring(props) {
+    return (
+        <div className="bg-white p-10 rounded-lg shadow flex justify-around">
+            <button type="button" className="bg-blue-500 hover:bg-blue-600 px-5 py-2 rounded text-white inline-flex focus:outline-none focus:ring-2 ring-blue-200">Primary</button>
+            <button type="button" className="bg-red-500 hover:bg-red-600 px-5 py-2 rounded text-white inline-flex focus:outline-none focus:ring-2 ring-red-200">Danger</button>
+            <button type="button" className="bg-lime-500 hover:bg-lime-600 px-5 py-2 rounded text-white inline-flex focus:outline-none focus:ring-2 ring-lime-200">Success</button>
+            <button type="button" className="bg-gray-800 hover:bg-gray-600 px-5 py-2 rounded text-white inline-flex focus:outline-none focus:ring-2 ring-gray-400">Secondary</button>
+        </div>
+    );
+}
+
+export default Ring;    
