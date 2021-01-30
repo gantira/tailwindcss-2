e72c76b142f8de9f991874aa4b3c469a1a0af0cd
Angga Gantira <angga.gantira@gmail.com>
2021-01-30 07:55:58 +0700

    2 Gunakan Warna Yang Baru

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 function Home(props) {
     return (
-        <div className="bg-red-500">
-            Welcome Next JS
+        <div className="flex items-center justify-center min-h-screen">
+            <div className="w-1/2">
+                <div className="bg-white shadow p-6 rounded">
+                    <div className="flex flex-wrap">
+                        <div className="bg-true-gray-500 w-10 h-10 rounded mr-2"></div>
+                        <div className="bg-lime-500 w-10 h-10 rounded mr-2"></div>
+                        <div className="bg-blue-500 w-10 h-10 rounded mr-2"></div>
+                        <div className="bg-emerald-500 w-10 h-10 rounded mr-2"></div>
+                        <div className="bg-cyan-500 w-10 h-10 rounded mr-2"></div>
+                        <div className="bg-red-500 w-10 h-10 rounded mr-2"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
